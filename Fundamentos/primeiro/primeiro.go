6578ab268e3e795dd47614b09cdb7d8ebfc16c52
CarolinaCedro <yotechgirl@gmail.com>
2022-10-08 19:48:34 -0300

    creat first program

--- Fundamentos/primeiro/primeiro.go
+++ Fundamentos/primeiro/primeiro.go
@@ -1,6 +1,12 @@
 package main
 
+/*
+Os códigos em Go são organizados em pacotes e para usá-los é necessário
+um ou vários imports.
+*/
 import "fmt"
+
+// A porta de entrada de um programa Go é a função main
 
 func main() {
 	fmt.Println("primeiro")
